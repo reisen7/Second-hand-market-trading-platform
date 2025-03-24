104962c7c6ff139337be9a6787c89653888f6a60
Reisen7 <89134690+reisen7@users.noreply.github.com>
2025-03-24 17:26:04 +0800

    Initial commit

---
+++ src/main/java/com/entity/model/XueshengModel.java
@@ -0,0 +1,158 @@
+package com.entity.model;
+
+import com.entity.XueshengEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.io.Serializable;
+ 
+
+/**
+ * 学生
+ * 接收传参的实体类  
+ *（实际开发中配合移动端接口开发手动去掉些没用的字段， 后端一般用entity就够用了） 
+ * 取自ModelAndView 的model名称
+ * @author 
+ * @email 
+ * @date 2025-03-24 21:06:03
+ */
+public class XueshengModel  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	 			
+	/**
+	 * 密码
+	 */
+	
+	private String mima;
+		
+	/**
+	 * 姓名
+	 */
+	
+	private String xingming;
+		
+	/**
+	 * 性别
+	 */
+	
+	private String xingbie;
+		
+	/**
+	 * 学生电话
+	 */
+	
+	private String xueshengdianhua;
+		
+	/**
+	 * 头像
+	 */
+	
+	private String touxiang;
+		
+	/**
+	 * 余额
+	 */
+	
+	private Float money;
+				
+	
+	/**
+	 * 设置：密码
+	 */
+	 
+	public void setMima(String mima) {
+		this.mima = mima;
+	}
+	
+	/**
+	 * 获取：密码
+	 */
+	public String getMima() {
+		return mima;
+	}
+				
+	
+	/**
+	 * 设置：姓名
+	 */
+	 
+	public void setXingming(String xingming) {
+		this.xingming = xingming;
+	}
+	
+	/**
+	 * 获取：姓名
+	 */
+	public String getXingming() {
+		return xingming;
+	}
+				
+	
+	/**
+	 * 设置：性别
+	 */
+	 
+	public void setXingbie(String xingbie) {
+		this.xingbie = xingbie;
+	}
+	
+	/**
+	 * 获取：性别
+	 */
+	public String getXingbie() {
+		return xingbie;
+	}
+				
+	
+	/**
+	 * 设置：学生电话
+	 */
+	 
+	public void setXueshengdianhua(String xueshengdianhua) {
+		this.xueshengdianhua = xueshengdianhua;
+	}
+	
+	/**
+	 * 获取：学生电话
+	 */
+	public String getXueshengdianhua() {
+		return xueshengdianhua;
+	}
+				
+	
+	/**
+	 * 设置：头像
+	 */
+	 
+	public void setTouxiang(String touxiang) {
+		this.touxiang = touxiang;
+	}
+	
+	/**
+	 * 获取：头像
+	 */
+	public String getTouxiang() {
+		return touxiang;
+	}
+				
+	
+	/**
+	 * 设置：余额
+	 */
+	 
+	public void setMoney(Float money) {
+		this.money = money;
+	}
+	
+	/**
+	 * 获取：余额
+	 */
+	public Float getMoney() {
+		return money;
+	}
+			
+}
